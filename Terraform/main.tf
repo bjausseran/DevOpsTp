97ac430508f29b9eb9b71fe6584b28ceda78fae8
bjausseran <benoit.jausseran@ynov.com>
2021-01-25 14:22:27 +0100

    remove sg id

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -42,12 +42,46 @@ resource "aws_security_group" "security_group_jausseran" {
     to_port     = 22
     protocol    = "tcp"
   }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = var.security_group_name
+  }
+}
+resource "aws_security_group" "security_group_jausseran" {
+  name		  = var.security_group_name
+  description = "jenkins security groups"
+  vpc_id      = var.vpc_id
+
   ingress {
     description = "web"
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
   }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = var.security_group_name
+  }
+}
+resource "aws_security_group" "security_group_jausseran" {
+  name		  = var.security_group_name
+  description = "jenkins security groups"
+  vpc_id      = var.vpc_id
+
   ingress {
     description = "jenkins"
     from_port   = 8080
