dcf18c7d38dca9cf6f413ffa70b2114a8bda63e1
bjausseran <benoit.jausseran@ynov.com>
2021-01-25 12:58:27 +0100

    secu file deleted

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -25,9 +25,45 @@ resource "aws_instance" "instance_jenkins_server_jausseran" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
   key_name      = var.key_name
-  vpc_security_group_ids = [ security_groups.aws_security_group.security_group_jenkins_jausseran.id ]
+  vpc_security_group_ids = [ aws_security_group.security_group_jenkins_jausseran.id ]
 
   tags = {
     Name = var.instance_name
+  }
+}
+
+resource "aws_security_group" "security_groups_jausseran" {
+  name		  = var.security_groups_name
+  description = "jenkins security groups"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+  }
+  ingress {
+    description = "web"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+  }
+  ingress {
+    description = "jenkins"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = var.security_groups_name
   }
 }
